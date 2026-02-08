3f38df11bfbf5dc3d383b198b753df0e0303bdd2
ARNAB DAS <raj713335@gmail.com>
2026-02-08 14:35:30 +0530

    :wrench: MCP Protocol

---
+++ LangChain/src/mcp_agent.py
@@ -0,0 +1,98 @@
+import asyncio
+from pathlib import Path
+
+from langchain.agents import create_agent
+from langchain.chat_models import init_chat_model
+from langchain_mcp_adapters.client import MultiServerMCPClient
+from langchain_mcp_adapters.tools import load_mcp_tools
+
+from dotenv import load_dotenv
+
+# PROJECT_ROOT = Path(__file__).resolve().parents[1]
+# MATH_SERVER = PROJECT_ROOT / "src" / "mcp_servers" / "math.py"
+#
+# print(MATH_SERVER)
+
+load_dotenv()
+
+
+async def main():
+    # MultiServerMCPClient can mix transports
+    # - stdio: spawn a local process
+    # - http: connect to a running server by URL
+
+    client = MultiServerMCPClient(
+        {
+            # "math": {
+            #     "command": "python",
+            #     "args": [str(MATH_SERVER)],
+            #     "transport": "stdio"
+            # },
+            "math": {
+                "url": "http://127.0.0.1:8000/mcp",
+                "transport": "http"
+            }
+        }
+    )
+
+    tools = await client.get_tools()
+
+    model = init_chat_model("openai:gpt-4.1-mini")
+
+    agent = create_agent(
+        model=model,
+        tools=tools,
+        system_prompt=(
+            "You are a helpful assistant. Use tools when needed."
+            "If math is required, call the math tools."
+        ),
+    )
+
+    r1 = await agent.ainvoke({"messages": "What is (3+5) x 12?"})
+
+    print("\n --- RESULT ---")
+    print(r1["messages"][-1].content)
+
+
+async def run_stateful():
+    # MultiServerMCPClient can mix transports
+    # - stdio: spawn a local process
+    # - http: connect to a running server by URL
+
+    client = MultiServerMCPClient(
+        {
+            # "math": {
+            #     "command": "python",
+            #     "args": [str(MATH_SERVER)],
+            #     "transport": "stdio"
+            # },
+            "math": {
+                "url": "http://127.0.0.1:8000/mcp",
+                "transport": "http"
+            }
+        }
+    )
+
+    async with client.session("math") as session:
+
+        tools = await load_mcp_tools(session)
+
+        model = init_chat_model("openai:gpt-4.1-mini")
+
+        agent = create_agent(
+            model=model,
+            tools=tools,
+            system_prompt=(
+                "You are a helpful assistant. Use tools when needed."
+                "If math is required, call the math tools."
+            ),
+        )
+
+        r1 = await agent.ainvoke({"messages": "What is (3+5) x 12?"})
+
+        print("\n --- RESULT ---")
+        print(r1["messages"][-1].content)
+
+
+if __name__ == "__main__":
+    asyncio.run(run_stateful())
---
+++ LangChain/src/mcp_servers/math.py
@@ -0,0 +1,21 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP("Math")
+
+
+@mcp.tool()
+def add(a: int, b: int) -> int:
+    """Add two integers"""
+    return a + b
+
+
+@mcp.tool()
+def multiply(a: int, b: int) -> int:
+    """Multiple two integers"""
+    return a * b
+
+
+if __name__ == "__main__":
+    # STDIO transport is perfect for local servers
+    #mcp.run(transport="stdio")
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
